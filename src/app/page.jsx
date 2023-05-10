7cabaa37ce112bcb7693ebe4c6e9633e388d55d4
Bryansss1 <gregoria1540@gmail.com>
2023-05-10 01:46:23 -0400

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from "./home.module.css"
-import title_claret from "../assets/icons/claret 1.png"
+import title_claret from "../assets/icons/claret_1.png"
 import icon_menu from "../assets/icons/apps.svg"
 import Menu_bar from '@/components/Menu_bar'
 import shield_claret from "../assets/icons/escudo.png"
